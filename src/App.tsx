5198cbf623a47674ffd66a85f2dffc997a32de1b
Juliano <67047882+JulianoJk@users.noreply.github.com>
2025-06-07 03:08:49 +0200

    Added toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
 import "./App.css";
-
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import { selectUser } from "./containers/auth/selectors";
-import logo from "./logo.svg";
+import ToggleColorMode from "./components/ui/ToggleColorMode.component";
 
 const App: FC = (): ReactElement => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-
   // TODO: You can access user data and now fetch user's playlists
   console.log(user);
 
+  const [mode, setMode] = useState<"light" | "dark">("dark");
+
+  const toggleMode = () => setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+
+  const theme = createTheme({ palette: { mode } });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ToggleColorMode mode={mode} setMode={toggleMode} />
+    </ThemeProvider>
   );
 };
 
